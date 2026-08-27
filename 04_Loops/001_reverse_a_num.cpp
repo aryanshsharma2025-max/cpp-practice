d7344e83c620698d1cd8d1ce19eaddd15a142a44
Aryansh Sharma <aryanshsharma2005@gmail.com>
2026-08-27 19:57:14 +0530

    feat: add program to reverse an integer using a loop

--- 04_Loops/001_reverse_a_num.cpp
+++ 04_Loops/001_reverse_a_num.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 void rev(int num)
 {
+    int rev = 0;
     while (num != 0)
     {
         int digit = num % 10;
-        int rev = rev * 10 + digit;
+        rev = rev * 10 + digit;
         num = num / 10;
     }
+    cout << rev;   
 }
 int main()
 {
