5d55c6df4e3c1dad6d2f390d1e2fdfe83fdb6a48
Aryansh Sharma <aryanshsharma2005@gmail.com>
2026-09-21 14:16:23 +0530

    new

---
+++ 08_Matrices/001_add_two_mat.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the size: ";
+    cin >> n;
+    int a[n] , b[n];
+    cout << "Enter the elements in A matrix: ";
+    for (int i = 0; i< n; i++)
+    {
+        cout << a[i] << endl;
+    }
+    cout << "Enter the elements in B matrix: ";
+    for (int i = 0; i< n; i++)
+    {
+        cout << b[i] << endl;
+    }
+
+}
