39292d2e0df5c2921814c5308dd78500bdc9291e
Aryansh Sharma <aryanshsharma2005@gmail.com>
2026-09-21 21:04:31 +0530

    feat: implement matrix scalar multiplication program

---
+++ 08_Matrices/003_scakar_mat_mul.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int rows , cols , scalar;
+    cout << "Enter number of rows: ";
+    cin >> rows;
+    cout << "Enter number of columns: ";
+    cin >> cols;
+    int matrix[rows][cols];
+    cout << "Enter elements of the matrix: \n";
+    for(int i = 0; i< rows; i++)
+    {
+        for(int j = 0; j< cols; j++)
+        {
+            cin >> matrix[i][j];
+        }
+    }
+    cout << "Enter scalar value: ";
+    cin >> scalar;
+    cout << "Result after scalar multiplication: "
+}
