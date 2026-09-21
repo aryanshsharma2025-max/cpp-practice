ac76f599e33b50f4c721480571d604b763affaf6
Aryansh Sharma <aryanshsharma2005@gmail.com>
2026-09-21 20:57:22 +0530

    fix: format code and improve matrix subtraction implementation

--- 08_Matrices/002_subtract_two_mat.cpp
+++ 08_Matrices/002_subtract_two_mat.cpp
@@ -1,8 +1,39 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
-int main ()
+
+int main()
 {
     int n;
     cout << "Enter the size of matrix: ";
     cin >> n;
+
+    vector<int> a(n), b(n), c(n);
+
+    cout << "Enter the elements in matrix A:\n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    cout << "Enter the elements in matrix B:\n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> b[i];
+    }
+
+    // subtraction
+    for (int i = 0; i < n; i++)
+    {
+        c[i] = a[i] - b[i];
+    }
+
+    cout << "Result after subtraction: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << c[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
